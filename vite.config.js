e83b9bba0be40dcbfe5b3d81610bf431f60cdd41
António Caetano <117670010+Ancaetano123@users.noreply.github.com>
2025-04-15 11:51:37 +0100

    github pages

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/<nome-do-repositorio>/", // Substitua <nome-do-repositorio> pelo nome do seu repositório no GitHub
+});
